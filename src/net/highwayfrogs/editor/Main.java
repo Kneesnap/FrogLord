acd171d6486fb55f4f934ddc5e12d843d6a03da7
Kneesnap <davidmenlo2@gmail.com>
2018-08-10 14:50:04 -0700

    Setup project.

---
+++ src/net/highwayfrogs/editor/Main.java
@@ -0,0 +1,12 @@
+package net.highwayfrogs.editor;
+
+/**
+ * The main class which starts the entire editor.
+ * Created by Kneesnap on 8/10/2018.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
