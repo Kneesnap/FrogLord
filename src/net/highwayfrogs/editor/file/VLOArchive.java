e091bf43290f3588e6173641c56398229e7dce34
Kneesnap <davidmenlo2@gmail.com>
2018-08-17 22:10:06 -0700

    VLO saving works.

--- src/net/highwayfrogs/editor/file/VLOArchive.java
+++ src/net/highwayfrogs/editor/file/VLOArchive.java
@@ -2,6 +2,7 @@ package net.highwayfrogs.editor.file;
 
 import lombok.Getter;
 import lombok.Setter;
+import net.highwayfrogs.editor.Constants;
 import net.highwayfrogs.editor.Utils;
 import net.highwayfrogs.editor.file.reader.DataReader;
 import net.highwayfrogs.editor.file.writer.DataWriter;
@@ -10,6 +11,8 @@ import java.awt.image.BufferedImage;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * VLOArchive - Image archive format created by VorgPC/Vorg2.
@@ -17,7 +20,9 @@ import java.nio.IntBuffer;
  * ABGR 8888 = 24 + 8.
  * Created by Kneesnap on 8/17/2018.
  */
+@Getter
 public class VLOArchive extends GameFile {
+    private List<GameImage> images = new ArrayList<>();
 
     public static final int TYPE_ID = 1;
     public static final int FLAG_TRANSLUCENT = 1;
@@ -30,6 +35,8 @@ public class VLOArchive extends GameFile {
 
 
     private static final String SIGNATURE = "2GRP";
+    private static final int IMAGE_INFO_BYTES = 24;
+    private static final int HEADER_SIZE = SIGNATURE.length() + 2 * Constants.INTEGER_SIZE;
     private static final int PIXEL_BYTES = 4;
 
     @Override
@@ -40,37 +47,61 @@ public class VLOArchive extends GameFile {
         reader.setIndex(reader.readInt());
 
         for (int i = 0; i < fileCount; i++) {
-
             GameImage image = new GameImage();
             image.setVramX(reader.readShort());
             image.setVramY(reader.readShort());
             image.setFullWidth(reader.readShort());
             image.setFullHeight(reader.readShort());
-
             int offset = reader.readInt();
-
             image.setTextureId(reader.readShort());
             image.setTexturePage(reader.readShort());
             image.setFlags(reader.readShort());
             image.setClutId(reader.readShort());
             image.setU(reader.readByte());
             image.setV(reader.readByte());
-            image.setIngameWidth(reader.readByte());
-            image.setIngameHeight(reader.readByte());
+            image.ingameWidth = reader.readByte();
+            image.ingameHeight = reader.readByte();
 
             reader.jumpTemp(offset);
             image.setImageBytes(reader.readBytes(image.getFullWidth() * image.getFullHeight() * PIXEL_BYTES));
             reader.jumpReturn();
+            this.images.add(image);
         }
     }
 
     @Override
     public void save(DataWriter writer) {
+        int imageCount = getImages().size();
+        writer.writeStringBytes(SIGNATURE);
+        writer.writeInt(imageCount);
+        writer.writeInt(writer.getIndex() + Constants.INTEGER_SIZE); // Offset to the VLO table info.
 
+        int offset = IMAGE_INFO_BYTES * imageCount + HEADER_SIZE;
+        for (GameImage image : getImages()) {
+            writer.writeShort(image.getVramX());
+            writer.writeShort(image.getVramY());
+            writer.writeShort(image.getFullWidth());
+            writer.writeShort(image.getFullHeight());
+            writer.writeInt(offset);
+            writer.writeShort(image.getTextureId());
+            writer.writeShort(image.getTexturePage());
+            writer.writeShort(image.getFlags());
+            writer.writeShort(image.getClutId());
+            writer.writeByte(image.getU());
+            writer.writeByte(image.getV());
+            writer.writeByte(image.ingameWidth);
+            writer.writeByte(image.ingameHeight);
+            offset += image.getImageBytes().length;
+        }
+
+        getImages().stream()
+                .map(GameImage::getImageBytes)
+                .forEach(writer::writeBytes);
     }
 
     @Getter
     @Setter
+    @SuppressWarnings("unused")
     public static class GameImage {
         private short vramX;
         private short vramY;
@@ -86,6 +117,8 @@ public class VLOArchive extends GameFile {
         private byte ingameHeight;
         private byte[] imageBytes;
 
+        private static final int MAX_DIMENSION = 256;
+
         /**
          * Export this game image as a BufferedImage.
          * @param trimEdges Should edges be trimmed so the textures are exactly how they appear in-game?
@@ -95,17 +128,20 @@ public class VLOArchive extends GameFile {
             int height = trimEdges ? getIngameHeight() : getFullHeight();
             int width = trimEdges ? getIngameWidth() : getFullWidth();
 
+            byte[] cloneBytes = new byte[imageBytes.length]; // We don't want to make any changes to the original array.
+            System.arraycopy(imageBytes, 0, cloneBytes, 0, cloneBytes.length);
+
             BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
             //ABGR -> BGRA
-            for (int temp = 0; temp < imageBytes.length; temp += PIXEL_BYTES) {
-                byte alpha = imageBytes[temp];
+            for (int temp = 0; temp < cloneBytes.length; temp += PIXEL_BYTES) {
+                byte alpha = cloneBytes[temp];
                 int alphaIndex = temp + PIXEL_BYTES - 1;
-                System.arraycopy(imageBytes, temp + 1, imageBytes, temp, alphaIndex - temp);
-                imageBytes[alphaIndex] = (byte) (0xFF - alpha); // Alpha needs to be flipped.
+                System.arraycopy(cloneBytes, temp + 1, cloneBytes, temp, alphaIndex - temp);
+                cloneBytes[alphaIndex] = (byte) (0xFF - alpha); // Alpha needs to be flipped.
             }
 
-            IntBuffer buffer = ByteBuffer.wrap(imageBytes)
+            IntBuffer buffer = ByteBuffer.wrap(cloneBytes)
                     .order(ByteOrder.LITTLE_ENDIAN)
                     .asIntBuffer();
 
@@ -115,12 +151,38 @@ public class VLOArchive extends GameFile {
             return image;
         }
 
+        /**
+         * Gets the in-game height of this image.
+         * @return ingameHeight
+         */
         public int getIngameHeight() {
-            return this.ingameHeight == 0 ? 256 : this.ingameHeight;
+            return this.ingameHeight == 0 ? MAX_DIMENSION : this.ingameHeight;
         }
 
+        /**
+         * Gets the in-game width of this image.
+         * @return ingameWidth
+         */
         public int getIngameWidth() {
-            return this.ingameWidth == 0 ? 256 : this.ingameWidth;
+            return this.ingameWidth == 0 ? MAX_DIMENSION : this.ingameWidth;
+        }
+
+        /**
+         * Set the in-game height of this image.
+         * @param height The in-game height.
+         */
+        public void setIngameHeight(int height) {
+            Utils.verify(height >= 0 && height <= MAX_DIMENSION, "Image height is not in the required range (0,%d].", MAX_DIMENSION);
+            this.ingameHeight = (byte) (height == MAX_DIMENSION ? 0 : height);
+        }
+
+        /**
+         * Set the in-game width of this image.
+         * @param width The in-game width.
+         */
+        public void setIngameWidth(int width) {
+            Utils.verify(width >= 0 && width <= MAX_DIMENSION, "Image width is not in the required range: (0,%d].", MAX_DIMENSION);
+            this.ingameWidth = (byte) (width == MAX_DIMENSION ? 0 : width);
         }
     }
 }
