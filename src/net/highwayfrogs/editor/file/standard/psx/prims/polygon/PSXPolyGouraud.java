53dc69e6b0ffbf67ec4f424fa5fb6e5edd2a7d21
Kneesnap <davidmenlo2@gmail.com>
2018-12-01 13:42:26 -0800

    Cleanup vertex coloring.

--- src/net/highwayfrogs/editor/file/standard/psx/prims/polygon/PSXPolyGouraud.java
+++ src/net/highwayfrogs/editor/file/standard/psx/prims/polygon/PSXPolyGouraud.java
@@ -21,6 +21,14 @@ public class PSXPolyGouraud extends PSXPolygon implements VertexColor {
     private PSXColorVector[] colors;
     @Setter private transient TextureEntry textureEntry;
 
+    private static final double SIZE_MULT = .5D;
+    private static final int[][] POSITION = {
+            {0, 0},
+            {0, 1},
+            {1, 0},
+            {1, 1}
+    };
+
     public PSXPolyGouraud(int verticeCount) {
         super(verticeCount);
         this.colors = new PSXColorVector[verticeCount];
@@ -52,15 +60,13 @@ public class PSXPolyGouraud extends PSXPolygon implements VertexColor {
 
     @Override //TODO: Shading.
     public void makeTexture(BufferedImage image, Graphics2D graphics) {
-        int i = 0;
-        for (int x = 0; x < image.getWidth(); x += (image.getWidth() / 2)) {
-            for (int y = 0; y < image.getHeight(); y += (image.getHeight() / 2)) {
-                if (i >= colors.length)
-                    continue;
+        for (int i = 0; i < colors.length; i++) {
+            graphics.setColor(colors[i].toColor());
 
-                graphics.setColor(colors[i++].toColor());
-                graphics.fillRect(x, y, image.getWidth() / 2, image.getHeight() / 2);
-            }
+            int width = (int) (image.getWidth() * SIZE_MULT);
+            int height = (int) (image.getHeight() * SIZE_MULT);
+            int[] pos = POSITION[i];
+            graphics.fillRect(pos[0] * width, pos[1] * height, width, height);
         }
     }
 }
