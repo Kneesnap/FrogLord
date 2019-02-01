4e60084a9336696dd51f498d5c2671d98a1f76c0
andyeder <andyeder@gmail.com>
2019-02-01 21:44:06 +0000

    Updated addData function to display floating point values of matrix data (converted from fixed point format). Also uses the new GUIEditorGrid.addVector3D function.

--- src/net/highwayfrogs/editor/file/map/entity/data/MatrixData.java
+++ src/net/highwayfrogs/editor/file/map/entity/data/MatrixData.java
@@ -2,6 +2,7 @@ package net.highwayfrogs.editor.file.map.entity.data;
 
 import lombok.Getter;
 import lombok.Setter;
+import net.highwayfrogs.editor.Utils;
 import net.highwayfrogs.editor.file.reader.DataReader;
 import net.highwayfrogs.editor.file.standard.psx.PSXMatrix;
 import net.highwayfrogs.editor.file.writer.DataWriter;
@@ -30,8 +31,23 @@ public class MatrixData extends EntityData {
 
     @Override
     public void addData(GUIEditorGrid editor) {
-        editor.addLabel("Matrix Position", Arrays.toString(matrix.getTransform()));
-        for (int i = 0; i < matrix.getMatrix().length; i++)
-            editor.addLabel("Matrix[" + i + "]", Arrays.toString(matrix.getMatrix()[i]));
+        float[] translation = new float[3];
+        float[] matrixRow = new float[3];
+
+        // Position information is in fixed point format, hence conversion to float representation.
+        for (int i = 0; i < matrix.getTransform().length; i++) {
+            translation[i] = Utils.unsignedIntToFloat(matrix.getTransform()[i]);
+        }
+        editor.addNormalLabel("Matrix Position");
+        editor.addVector3D(translation, 30.0);
+
+        // Transform information is in fixed point format, hence conversion to float representation.
+        editor.addNormalLabel("Matrix Transformation");
+        for (int i = 0; i < matrix.getMatrix().length; i++) {
+            for (int j = 0; j < matrix.getMatrix().length; j++) {
+                matrixRow[j] = Utils.unsignedShortToFloatNBits(matrix.getMatrix()[i][j], 12);
+            }
+            editor.addVector3D(matrixRow, 25.0 + ((i == (matrix.getMatrix().length - 1)) ? (5.0) : (0.0)));
+        }
     }
 }
